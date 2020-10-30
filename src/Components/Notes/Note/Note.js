602905538f159256ee58aa84282e9b1bec40dfe6
Byter <avishai.kk@gmail.com>
2020-10-31 00:14:11 +0200

    added a draggable note

--- src/Components/Notes/Note/Note.js
+++ src/Components/Notes/Note/Note.js
@@ -1,5 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Draggable from "react-draggable";
 
-const Note = () => {};
+import classes from "./Note.module.css";
+
+const Note = props => {
+   let content = props.content.slice(0, 175);
+   content += "...";
+
+   return (
+      <Draggable bounds="parent">
+         <div className={classes.Note}>
+            <h3 className={classes.Title}>{props.title}</h3>
+            <p className={classes.Content}>{content}</p>
+         </div>
+      </Draggable>
+   );
+};
+
+Note.propTypes = {
+   title: PropTypes.string,
+   content: PropTypes.string,
+};
 
 export default Note;
