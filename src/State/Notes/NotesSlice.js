62adfbde186d56beef47f360883e419cf5a69d96
Ethan Person <ethans.2nd.one@gmail.com>
2020-10-30 13:31:42 -0600

    add redux toolkit

---
+++ src/State/Notes/NotesSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
+
+// If already familiar with redux, redux-toolkit speeds up the development proccess
+// https://redux-toolkit.js.org/api/createSlice
+
+/**
+* NOTES STATE EXAMPLE
+{
+  "838748c7-b06c-4e07-9f8d-85f22986d1db": {
+    id: "838748c7-b06c-4e07-9f8d-85f22986d1db",
+    text: "Some String"
+  }
+}
+*/
+
+const initialState = {};
+
+const notesSlice = createSlice({
+	name: 'notes',
+	initialState,
+	reducers: {
+		createNote(state, action) {
+			const newNoteId = uuid();
+			const note = action.payload;
+			state[newNoteId] = { ...note, id: newNoteId };
+		},
+		deleteNote(state, action) {
+			const note = action.payload;
+			const deletingNoteId = note.id;
+			delete state[deletingNoteId];
+		},
+		updateNote(state, action) {
+			const note = action.payload;
+			state[note.id] = { ...note };
+		},
+	},
+});
+
+export const { createNote, deleteNote, updateNote } = notesSlice.actions;
+export default notesSlice.reducer;
---
+++ src/State/index.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import noteReducer from './Notes/NotesSlice';
+
+const rootReducer = {
+	notes: noteReducer,
+};
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
