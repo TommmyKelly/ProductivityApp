878b1e69add18e00567f928af1597ff83f60b6d4
Byter <avishai.kk@gmail.com>
2020-10-31 00:44:40 +0200

    made sure everything is working correctly after the conflict. I added the NoteBoard to the app.js and change the curser when dragging a note

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Provider } from "react-redux";
 import store from "./State";
 
 import NavigationBar from "./Components/NavigationBar/NavigationBar";
+import NoteBoard from "./Containers/NoteBoard/NoteBoard";
 
 function App() {
    return (
       <Provider store={store}>
          <BrowserRouter>
             <NavigationBar />
+            <NoteBoard />
          </BrowserRouter>
       </Provider>
    );
