fd04d557d28283d7d8a492cf39652748a80d609b
Byter <avishai.kk@gmail.com>
2020-10-31 10:17:33 +0200

    Found a way to save the note's position and commented it out

--- src/Components/Notes/Note/Note.js
+++ src/Components/Notes/Note/Note.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Draggable from "react-draggable";
 
 import classes from "./Note.module.css";
 
+// NOTE:
+// The state will be used to save the data so don't delete!!
+
 const Note = props => {
+   // const [position, setPosition] = useState({ x: 0, y: 0 });
+
    let content = props.content.slice(0, 175);
    content += "...";
 
+   // const onDragStop = event => {
+   //    setPosition({ x: event.x, y: event.y });
+   // };
+
    return (
-      <Draggable bounds="parent">
+      <Draggable bounds="parent" /*onStop={onDragStop}*/>
          <div className={classes.Note}>
             <h3 className={classes.Title}>{props.title}</h3>
             <p className={classes.Content}>{content}</p>
